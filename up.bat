6f1c0da03d4d73a0ade1d729ccefe34d06267069
Daxi90 <hdateam90@gmail.com>
2024-04-03 16:22:03 +0200

    Commi with up.bat

--- up.bat
+++ up.bat
@@ -1,4 +1,4 @@
 git add .
 git commit -m "%*"
 git push
-chrome "https://www.pythonanywhere.com/user/daxi90/consoles/33162997/"
+Start-Process chrome "https://www.pythonanywhere.com/user/daxi90/consoles/33162997/"
